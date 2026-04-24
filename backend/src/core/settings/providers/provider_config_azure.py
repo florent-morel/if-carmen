5b198198e745d522004891c57fbcd1a947b930e2
Florent MOREL <florent.morel@amadeus.com>
2026-04-24 16:28:37 +0200

    .

--- backend/src/core/settings/providers/provider_config_azure.py
+++ backend/src/core/settings/providers/provider_config_azure.py
@@ -14,13 +14,6 @@ class Provider_Config_Azure(AbstractProviderConfig):
     Configuration class for Azure Provider.
     """
 
-    def __init__(self):
-        self.pue: float
-        self.electricity_ratios: dict[str: float]
-        self.storage_embodied: dict[str: int]
-        self.storage_replication_factors: dict[str: int]
-        self.disk_sku_size_mapping: dict[str: int]
-
     def get_pue(self) -> float | None:
         return self.pue
 
