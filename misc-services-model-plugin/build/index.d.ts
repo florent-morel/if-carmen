f690a8dc8cf44bd805a22aa17a8f2fd7c6bed5db
Robin <castellon.robin@gmail.com>
2026-05-27 15:42:01 +0200

    New misc-services-model build

---
+++ misc-services-model-plugin/build/index.d.ts
@@ -0,0 +1,1 @@
+export * from './lib/misc-services-model-plugin';
---
+++ misc-services-model-plugin/build/lib/misc-services-model-plugin/index.d.ts
@@ -0,0 +1,16 @@
+import { PluginParams, ConfigParams } from '@grnsft/if-core/types';
+export declare const MiscServicesModelPlugin: (config: ConfigParams | undefined, parametersMetadata: import("@grnsft/if-core/types").PluginParametersMetadata, mapping: import("@grnsft/if-core/types").MappingParams) => {
+    metadata: {
+        inputs: {
+            [x: string]: {
+                description: string;
+                unit: string;
+                "aggregation-method": import("@grnsft/if-core/types").AggregationOptions;
+            };
+        };
+        outputs: import("@grnsft/if-core/types").ParameterMetadata;
+    };
+    execute: (inputs: PluginParams[]) => Promise<{
+        [x: string]: any;
+    }[]>;
+};
