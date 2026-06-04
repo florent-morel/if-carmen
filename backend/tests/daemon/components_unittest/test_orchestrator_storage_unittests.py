284b5822578f2fffd315c9b063c4ce589da9029d
Robin <castellon.robin@gmail.com>
2026-06-04 17:39:46 +0200

    New stuff

--- backend/tests/daemon/components_unittest/test_orchestrator_storage_unittests.py
+++ backend/tests/daemon/components_unittest/test_orchestrator_storage_unittests.py
@@ -13,10 +13,6 @@ from backend.src.daemon.carbon_daemon_orchestrator import CarbonDaemonOrchestrat
 from backend.src.daemon.processors.processor_storage import Processor_Storage
 from backend.src.common.carmen_exception import CarmenException, DataFetchError
 from backend.src.common.errors import ERRORS, ErrorCode
-from backend.src.daemon.carbon_daemon_orchestrator import (
-    CarbonDaemonOrchestrator,
-    CarbonDaemonResult,
-)
 
 from backend.src.common.constants import (
     HOURLY_INTERVAL_SECONDS,
@@ -197,6 +193,8 @@ class TestCarbonDaemonOrchestratorStorage(unittest.TestCase):
         """
         Test daemon execution when reader raises an exception.
         """
+        mock_reader = MagicMock()
+        mock_reader.read.side_effect = CarmenException(ErrorCode.UNKNOWN_ERROR, "Reader failed")
 
         mock_ioc_util_resolve.return_value = IFStorageService(DAILY_SECONDS)
 
@@ -205,8 +203,10 @@ class TestCarbonDaemonOrchestratorStorage(unittest.TestCase):
 
         mock_processor = MagicMock()
         mock_processor.resource_type = ResourceType.STORAGE
-        mock_processor.read.side_effect = CarmenException(ErrorCode.UNKNOWN_ERROR, details="Test Reader failed")
+        mock_processor.read.side_effect = CarmenException(ErrorCode.UNKNOWN_ERROR, "Reader failed")
         mock_processor.run.return_value = None
+
+        logger.info(f"Mock processor: {mock_processor}")
 
         orchestrator = CarbonDaemonOrchestrator(self.mock_config, [mock_processor])
         mock_write_report.return_value = None
@@ -215,18 +215,20 @@ class TestCarbonDaemonOrchestratorStorage(unittest.TestCase):
 
         logger.info(f"Carbon Daemon Result: {carbonDaemonResult}")
 
-        self.assertIsInstance(carbonDaemonResult, CarbonDaemonResult)
+        # resultStorage = carbonDaemonResult.dict_resource_result[ResourceType.STORAGE]
+        # self.assertIsNone(carbonDaemonResult.dict_resource_result[ResourceType.STORAGE])
+
+        # self.assertIsInstance(resultStorage, ResourceTypeResult)
         self.assertFalse(carbonDaemonResult.success)
         self.assertIsNotNone(carbonDaemonResult.list_exceptions)
         logger.info(f"list_exceptions: {carbonDaemonResult.list_exceptions}")
 
         for exception in carbonDaemonResult.list_exceptions:
             logger.info(f"Exception: {exception.error_code}, \n details: {exception.formatted_string}")
-            self.assertEqual(exception.error_code, ErrorCode.UNKNOWN_ERROR)
             self.assertIn(
                 "Unexpected error reading file", exception.details
             )
-            self.assertIn("Test Reader failed", exception.details)
+            self.assertIn("Reader failed", exception.details)
 
 
 #     @patch("backend.src.daemon.carbon_daemon.register_models")
