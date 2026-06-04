be9f648d499eaccb975746cb57adcb437f5cbf30
Florent MOREL <florent.morel@amadeus.com>
2026-06-04 17:43:03 +0200

    .

--- backend/tests/daemon/readers/test_reader_storage.py
+++ backend/tests/daemon/readers/test_reader_storage.py
@@ -14,7 +14,6 @@ from backend.tests.daemon import mock_data
 from backend.src.common.constants import (
     CSV_PATH,
     CSV_FILE_ENCODING,
-    SOURCE_PROVIDER,
     HOURLY_INTERVAL_SECONDS,
     SOURCE_RESOURCE_ID,
     SOURCE_PROVIDER,
