1eaee9309477f1b13b9f879ef47d9feb6598c765
Florent MOREL <florent.morel@amadeus.com>
2026-06-04 11:17:21 +0200

    First implem of custom columns

--- backend/src/daemon/readers/abstract_reader.py
+++ backend/src/daemon/readers/abstract_reader.py
@@ -97,3 +97,11 @@ class AbstractReader(ABC):
                 provider,
                 count,
             )
+
+    def process_custom_columns(self, resource: Resource, row: dict) -> None:
+        """
+        Process custom columns from input file.
+        """
+        for column_name, column_value in row.items():
+            if column_name:
+                resource.dict_custom_columns[column_name] = column_value
